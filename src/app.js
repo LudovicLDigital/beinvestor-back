a96219c0ebf12797e746a9d69e1d348f886256ff
lulauriche <lulauriche>
2020-03-25 20:10:44 +0100

    :bug:<feat>(HTTPS) need a domain to work and generate the certificate correctly

--- src/app.js
+++ src/app.js
@@ -2,7 +2,14 @@ require('dotenv').config();
 const hostname = 'localhost';
 const port = 3000;
 const app = require('./shared/config/main-app-router');
-
-app.listen(port, hostname, function(){
-    console.log("Serve starting on -----> http://"+ hostname +":"+port+"\n");
-});
+const fs = require('fs');
+const key = fs.readFileSync('encryption/private.key');
+const cert = fs.readFileSync( 'encryption/primary.crt' );
+const ca = fs.readFileSync( 'encryption/intermediate.crt' );
+const https = require('https');
+const options = {
+    key: key,
+    cert: cert,
+    ca: ca
+};
+https.createServer(options, app).listen(port);
